7318370b78ebae024461f3e61d2c76ea653f621f
Justin Mayfield <tooker@gmail.com>
2016-11-05 15:03:25 -0600

    Experiment with braille

--- sparkline/sparkline.py
+++ sparkline/sparkline.py
@@ -1,8 +1,38 @@
 # vim: set fileencoding=utf-8 :
-import math, os, re, string, sys
+from __future__ import print_function
 
+import itertools
+import math
+import os
+import re
+import sys
+
+# Eight unicode characters of (nearly) steadily increasing height.
 spark_chars = u"▁▂▃▄▅▆▇█"
-"""Eight unicode characters of (nearly) steadily increasing height."""
+
+braile_ref = '''
+    ⡀⡁⡂⡃⡄⡅⡆⡇⡈⡉⡊⡋⡌⡍⡎⡏
+    ⡐⡑⡒⡓⡔⡕⡖⡗⡘⡙⡚⡛⡜⡝⡞⡟
+    ⡠⡡⡢⡣⡤⡥⡦⡧⡨⡩⡪⡫⡬⡭⡮⡯
+    ⡰⡱⡲⡳⡴⡵⡶⡷⡸⡹⡺⡻⡼⡽⡾⡿
+    ⢀⢁⢂⢃⢄⢅⢆⢇⢈⢉⢊⢋⢌⢍⢎⢏
+    ⢐⢑⢒⢓⢔⢕⢖⢗⢘⢙⢚⢛⢜⢝⢞⢟
+    ⢠⢡⢢⢣⢤⢥⢦⢧⢨⢩⢪⢫⢬⢭⢮⢯
+    ⢰⢱⢲⢳⢴⢵⢶⢷⢸⢹⢺⢻⢼⢽⢾⢿
+    ⣀⣁⣂⣃⣄⣅⣆⣇⣈⣉⣊⣋⣌⣍⣎⣏
+    ⣐⣑⣒⣓⣔⣕⣖⣗⣘⣙⣚⣛⣜⣝⣞⣟
+    ⣠⣡⣢⣣⣤⣥⣦⣧⣨⣩⣪⣫⣬⣭⣮⣯
+    ⣰⣱⣲⣳⣴⣵⣶⣷⣸⣹⣺⣻⣼⣽⣾⣿
+'''
+
+compressed_dots = (
+    ' ⢀⢠⢰⢸',
+    '⡀⣀⣠⣰⣸',
+    '⡄⣄⣤⣴⣼',
+    '⡆⣆⣦⣶⣾',
+    '⡇⣇⣧⣷⣿',
+)
+
 
 def _convert_to_float(i):
     try:
@@ -10,9 +40,29 @@ def _convert_to_float(i):
     except:
         return None
 
+
+def dotify(series):
+    """ Use dot based graphing (denser). """
+    # quantify data into 0->4 ints.
+    low = min(series)
+    normalized = [x - low for x in series]
+    high = max(normalized) or 1
+    quantum = len(compressed_dots[0]) - 1
+    quantized = [round((x / high) * quantum) for x in normalized]
+    if len(quantized) % 2 == 1:
+        qiter = itertools.chain(quantized, [0])
+    else:
+        qiter = iter(quantized)
+    buf = []
+    for x1 in qiter:
+        x2 = next(qiter)
+        buf.append(compressed_dots[x1][x2])
+    return ''.join(buf)
+
+
 def sparkify(series):
     u"""Converts <series> to a sparkline string.
-    
+
     Example:
     >>> sparkify([ 0.5, 1.2, 3.5, 7.3, 8.0, 12.5, 13.2, 15.0, 14.2, 11.8, 6.1,
     ... 1.9 ])
@@ -54,6 +104,7 @@ def guess_series(input_string):
         ] if i is not None and not math.isnan(i) and not math.isinf(i)
     ])
 
+
 def main():
     u"""Reads from command line args or stdin and prints a sparkline from the
     data.  Requires at least 2 data points as input.
@@ -74,10 +125,10 @@ def main():
         help="Display the version number and exit."
     )
     args = parser.parse_args()
-    
+
     if args.version:
         version = require("pysparklines")[0].version
-        print (version)
+        print(version)
         sys.exit(0)
 
     if os.isatty(0) and not args.data:
@@ -94,7 +145,7 @@ def main():
         sys.stderr.write("Could not convert input data to valid sparkline\n")
         sys.exit(1)
 
-    print (output.encode('utf-8', 'ignore'))
+    print(output.encode('utf-8', 'ignore'))
 
 if __name__ == "__main__":
     main()
